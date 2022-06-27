8eaf35b44507a08d45d6837308810a123cda32a2
Rushikesh Suryawanshi <79930026+Rushiu1@users.noreply.github.com>
2022-06-27 20:46:30 +0530

    Add files via upload

---
+++ money_machine.py
@@ -0,0 +1,34 @@
+class MoneyMachine:
+
+    CURRENCY = "₹"
+
+    
+
+    def __init__(self):
+        self.profit = 0
+        self.money_received = 0
+
+    def report(self):
+        """Prints the current profit"""
+        print(f"Money: {self.profit}")
+
+    def process_coins(self):
+        """Returns the total calculated from coins inserted."""
+        print("Please insert Money.")
+        self.money_received = int(input(f"How many ruppee: ")) 
+        return self.money_received
+
+    def make_payment(self, cost):
+        """Returns True when payment is accepted, or False if insufficient."""
+        self.process_coins()
+        if self.money_received >= cost:
+            change = round(self.money_received - cost, 2)
+            print(f"Here is {change}₹ in change.")        
+            self.profit += cost
+            self.money_received = 0
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            self.money_received = 0
+            return False
+        
